f7393eb2e4358028667295f826797770c58f31c6
Nirvik Pande <nirvikpande@gmail.com>
2025-02-19 10:17:47 -0600

    Built adder page

---
+++ CollegeApplicationTracker/AddNewApplication.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CollegeApplicationTracker
+{
+    partial class AddNewApplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(334, 40);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Add New Application";
+            // 
+            // AddNewApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(819, 548);
+            this.Controls.Add(this.label2);
+            this.Name = "AddNewApplication";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+    }
+}
